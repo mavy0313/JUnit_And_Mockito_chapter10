952d8cd182351d607d810886b2d3c8a4e810c886
Maxim Vypov <maxim.vypov@netcracker.com>
2018-03-06 09:50:25 +0400

    Added more tests

--- src/main/java/exercises/stack/Stack.java
+++ src/main/java/exercises/stack/Stack.java
@@ -3,9 +3,12 @@ package exercises.stack;
 public class Stack {
 
     private final int size;
+    private int[] array;
+    private int currentIndex;
 
     public Stack(int size) {
         this.size = size;
+        array = new int[size];
     }
 
     public int size() {
@@ -13,9 +16,15 @@ public class Stack {
     }
 
     public void push(int i) {
+        array[currentIndex + 1] = i;
     }
 
-    public boolean contains(int i) {
+    public boolean contains(int element) {
+        for (int i = 0; i < size; i++) {
+            if (element == array[i]) {
+                return true;
+            }
+        }
         return false;
     }
 }
--- src/test/java/exercises/stack/StackTest.java
+++ src/test/java/exercises/stack/StackTest.java
@@ -2,34 +2,39 @@ package exercises.stack;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class StackTest {
 
     @Test
-    public void shouldCreateStack() {
-        Stack stack = new Stack(3);
-        assertNotNull(stack);
-    }
-
-    @Test
-    public void shouldHaveSize3() {
+    public void shouldBeCreatedAndHaveCorrectSize() {
         Stack stack = new Stack(3);
         assertEquals(3, stack.size());
     }
 
     @Test
-    public void shouldHaveSize2() {
-        Stack stack = new Stack(2);
-        assertEquals(2, stack.size());
-    }
-
-    @Test
-    public void shouldPushElement() {
-        Stack stack = new Stack(3);
+    public void shouldContainPushedElement() {
+        Stack stack = new Stack(1);
         stack.push(1);
         assertTrue(stack.contains(1));
     }
+
+    @Test
+    public void shouldContain2PushedElements() {
+        Stack stack = new Stack(3);
+        stack.push(1);
+        stack.push(2);
+
+        assertTrue(stack.contains(1));
+        assertTrue(stack.contains(2));
+    }
+
+    @Test
+    public void shouldNotContainNotPushedElement() {
+        Stack stack = new Stack(3);
+        stack.push(1);
+        assertFalse(stack.contains(2));
+    }
+
+
 }
